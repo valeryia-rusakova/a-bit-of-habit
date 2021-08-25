240f7c6cbb26851fdbb1eac182e30dc8b009ac17
Valeryia <valeryia@wsb-042-74b.local>
2021-08-25 14:08:32 +0300

    Connect media files

--- development/urls.py
+++ development/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("project.urls")),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
