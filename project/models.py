bac5056f990645a0e1a2d3a60543214e30c66ab8
Valeryia <valeryia@wsb-042-74b.local>
2021-08-27 14:16:01 +0300

    Refactore signals to separate files

--- project/models.py
+++ project/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from enums import AchievementLevel, HabitType
 
 
@@ -105,13 +103,5 @@ class Profile(models.Model):
     birth_date = models.DateField(null=True, blank=True)
     image = models.ForeignKey(Image, null=True, blank=True, on_delete=models.SET_NULL)
 
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    def __str__(self):
+        return self.user.get_username()
