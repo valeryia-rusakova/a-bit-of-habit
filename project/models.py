7464e81fa3d4ed6c159c01a474cc64dc1f726053
Valeryia <valeryia@wsb-042-74b.local>
2021-08-27 17:40:34 +0300

    Delete Profile model

--- project/models.py
+++ project/models.py
@@ -98,20 +98,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.header
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    birth_date = models.DateField(null=True, blank=True)
-    image = models.ForeignKey(Image, null=True, blank=True, on_delete=models.SET_NULL)
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
