25bcf961c1f94847bd10ed6b28b191f8b3895842
valeria-rusakova-learnpython <valeria.rusakova.learnpython@gmail.com>
2021-11-22 18:02:23 +0300

    User account migration and changes in settings

--- project/admin.py
+++ project/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from project.models import Achievement, AchievementUser, Comment, Habit, HabitUser, Image, Post
+from project.models import Achievement, AchievementUser, Comment, Habit, HabitUser, Image, Post, UserAccount
 
 admin.site.register(Achievement)
 admin.site.register(AchievementUser)
@@ -8,5 +8,6 @@ admin.site.register(Habit)
 admin.site.register(HabitUser)
 admin.site.register(Image)
 admin.site.register(Post)
+admin.site.register(UserAccount)
 
 
