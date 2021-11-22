4acc50410f7e2ca078cdb34bb7d797d957d0d7eb
valeria-rusakova-learnpython <valeria.rusakova.learnpython@gmail.com>
2021-11-22 17:57:11 +0300

    Create UserAccount model & manager

--- project/models.py
+++ project/models.py
@@ -1,5 +1,12 @@
+from datetime import timedelta, datetime
+
+import jwt
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, PermissionsMixin, UserManager
+from django.contrib.auth.hashers import make_password
+
+from development import settings
 from enums import AchievementLevel, HabitType
 
 
@@ -20,7 +27,7 @@ class Achievement(models.Model):
 
 class AchievementUser(models.Model):
     achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey("UserAccount", on_delete=models.CASCADE)
     description = models.TextField(verbose_name='achievement description')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -30,7 +37,7 @@ class AchievementUser(models.Model):
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey("UserAccount", on_delete=models.CASCADE)
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
     body = models.TextField(verbose_name='comment text')
     created_at = models.DateTimeField(auto_now_add=True)
@@ -62,7 +69,7 @@ class Habit(models.Model):
 
 class HabitUser(models.Model):
     habit = models.ForeignKey(Habit, null=True, on_delete=models.SET_NULL)
-    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey("UserAccount", null=True, on_delete=models.SET_NULL)
     start_date = models.DateField(auto_created=True)
     days_checked = models.IntegerField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -86,7 +93,7 @@ class Image(models.Model):
 class Post(models.Model):
     header = models.CharField(verbose_name='post name', unique=True, max_length=30)
     body = models.TextField(verbose_name='post text', blank=False, null=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey("UserAccount", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -96,3 +103,71 @@ class Post(models.Model):
 
     def __str__(self):
         return self.header
+
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if username is None:
+            raise TypeError('Users must have a username.')
+
+        if email is None:
+            raise TypeError('Users must have an email address.')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        password = make_password(password)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, email, password):
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.username
+
+    def get_short_name(self):
+        return self.username
+
+    def get_username(self):
+        return self.username
+
+    @property
+    def token(self):
+        return self._generate_jwt_token()
+
+    def _generate_jwt_token(self):
+        dt = datetime.now() + timedelta(days=1)
+
+        token = jwt.encode({
+            'id': self.pk,
+            'exp': int(dt.timestamp())
+        }, settings.SECRET_KEY, algorithm='HS256')
+
+        return token
+
