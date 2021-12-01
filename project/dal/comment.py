ccd95df9056a72c7ea459bfdc2e142cdfd1be287
valeria-rusakova-learnpython <valeria.rusakova.learnpython@gmail.com>
2021-12-02 01:58:49 +0300

    Delete useless view, dal, controller functions and urls. Change permissions

--- project/dal/comment.py
+++ project/dal/comment.py
@@ -1,28 +1,11 @@
-from django.shortcuts import get_object_or_404
 from project.models import Comment
 
 
 class CommentDAL:
     @staticmethod
-    def retrieve_comment(pk):
-        return get_object_or_404(Comment, pk=pk)
-
-    @staticmethod
     def get_comments_list():
         return Comment.objects.all()
 
     @staticmethod
-    def delete_comment(pk):
-        comment = get_object_or_404(Comment, pk=pk)
-        comment.delete()
-
-    @staticmethod
     def insert_comment(object_data: dict):
         Comment.objects.create(**object_data)
-
-    @staticmethod
-    def update_comment(object_data: dict, pk):
-        comment = get_object_or_404(Comment, pk=pk)
-        comment.body = object_data['body']
-        comment.post = object_data['post']
-        comment.save()
