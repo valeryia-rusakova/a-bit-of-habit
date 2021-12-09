a04f2cde2d84df68a5a66a6ae60d3c24499b8fc8
valeria-rusakova-learnpython <valeria.rusakova.learnpython@gmail.com>
2021-12-10 02:32:25 +0300

    Refactor signals: move all logic to the upper layer

--- project/controllers/achievement_user.py
+++ project/controllers/achievement_user.py
@@ -1,4 +1,6 @@
+from project.dal.achievement import AchievementDAL
 from project.dal.achievement_user import AchievementUserDAL
+from project.dal.habit import HabitDAL
 
 
 class AchievementUserController:
@@ -6,3 +8,14 @@ class AchievementUserController:
 
     def get_user_achievements(self, user):
         return self.dal.get_user_achievements(user)
+
+    def create_achievement_user(self, days_checked, habit, user):
+        achievement = AchievementDAL.get_achievement_by_amount_to_reach(days_checked)
+        habit = HabitDAL.retrieve_habit(habit)
+        description = f"You’ve been keeping {habit.name} for {achievement.amount_to_reach} days"
+        data = {
+            'user': user,
+            'achievement': achievement,
+            'description': description
+        }
+        self.dal.insert_user_achievement(data)
--- project/dal/achievement_user.py
+++ project/dal/achievement_user.py
@@ -5,3 +5,7 @@ class AchievementUserDAL:
     @staticmethod
     def get_user_achievements(user):
         return AchievementUser.objects.filter(user=user.id).select_related('achievement')
+
+    @staticmethod
+    def insert_user_achievement(object_data: dict):
+        AchievementUser.objects.create(**object_data)
